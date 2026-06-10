0add6716cf9f88d0980badd42f499ca5af1e68d8
Tony <16115291+Toysoldier34@users.noreply.github.com>
2026-06-09 21:18:51 -0700

    Minor styling tweaks

--- blackjack/src/MainPage.tsx
+++ blackjack/src/MainPage.tsx
@@ -311,7 +311,7 @@ interface PlayingCardProps { card: Card; }
 const PlayingCard = ({card}: PlayingCardProps) => {
   return (
     <div className="hover-3d my-12 mx-2 cursor-pointer">
-      <div className="card w-50 h-70 bg-black text-white border-2 border-white bg-[radial-gradient(circle_at_bottom_left,#ffffff04_35%,transparent_36%),radial-gradient(circle_at_top_right,#ffffff04_35%,transparent_36%)] bg-size-[4.95em_4.95em]">
+      <div className="card w-50 h-70 bg-gray-900 text-white border-2 border-white bg-[radial-gradient(circle_at_bottom_left,#ffffff04_35%,transparent_36%),radial-gradient(circle_at_top_right,#ffffff04_35%,transparent_36%)] bg-size-[4.95em_4.95em]">
         <div className="card-body">
           <div className="flex mb-10">
             <div className="font-bold text-3xl">{card.value}</div>
